895bae73d4f4d734facf8bb887cd8e3ef8ce5614
dimonioi4 <dimonioi4@yandex.ru>
2021-01-22 13:38:39 +0300

    avia added

---
+++ avia.java
@@ -0,0 +1,8 @@
+class avia {
+    public static void main(String[] args) {
+        int percent = 20;
+        long amount = 12620L;
+        long bonus = amount / (long)percent;
+        System.out.println(bonus);
+    }
+}
