d9fae05c6ffa93d0ea1ad7c35ff260747269b9f4
dimonioi4 <58983231+dimonioi4@users.noreply.github.com>
2021-01-26 14:16:34 +0300

    Update Avia.java

--- Avia.java
+++ Avia.java
@@ -2,7 +2,7 @@ class Avia {
     public static void main(String[] args) {
         int percent = 20;
         long amount = 12620L;
-        long bonus = amount / (long)percent;
+        long bonus = amount / percent;
         System.out.println(bonus);
     }
 }
